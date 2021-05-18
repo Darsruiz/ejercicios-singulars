27c350c359f5490e38d2ba14bba83192065aa014
Darsruiz <darsruiz@gmail.com>
2021-05-18 19:27:12 +0200

    Finished Tour of Heroes

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Dr Not Evil' },
+  { id: 14, name: 'Celerity' },
+  { id: 15, name: 'Magenta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Drama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
